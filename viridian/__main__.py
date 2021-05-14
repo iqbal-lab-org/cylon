01f0aa729bbd82141ce9947653bfaacabcf07137
Martin Hunt <martin.g.hunt@gmail.com>
2021-05-14 10:07:14 +0100

    Add required option 'tech', and change some default values

--- viridian/__main__.py
+++ viridian/__main__.py
@@ -5,6 +5,23 @@ import logging
 import viridian
 
 
+TECH_DEPENDENT_DEFAULTS = {
+    "minimap_opts": {"illumina": "-t 1 -x sr", "ont": "-t 1 -x map-ont"},
+    "target_coverage": {"illumina": 150, "ont": 250},
+    "min_read_length": {"illumina": 50, "ont": 200},
+}
+
+
+def tech_dependent_usage_default_string(option):
+    return "; ".join([f"{k}:{v}" for k, v in TECH_DEPENDENT_DEFAULTS[option].items()])
+
+
+def set_tech_dependent_args(args):
+    for option in TECH_DEPENDENT_DEFAULTS:
+        if getattr(args, option) is None:
+            setattr(args, option, TECH_DEPENDENT_DEFAULTS[option][args.tech])
+
+
 def main(args=None):
     parser = argparse.ArgumentParser(
         prog="viridian",
@@ -25,9 +42,15 @@ def main(args=None):
     subparser_assemble = subparsers.add_parser(
         "assemble",
         help="Reference-guided assembly from reads",
-        usage="viridian assemble [options] <ref_fasta> <amplicons_bed> <outdir>",
+        usage="viridian assemble [options] <tech> <ref_fasta> <amplicons_bed> <outdir>",
         description="Reference-guided assembly from reads",
         epilog="Required: --bam, or --reads_to_map, or both --reads_to_map and --mates_to_map",
+    )
+    subparser_assemble.add_argument(
+        "tech",
+        help="Sequencing technology. Must be 'ont' or 'illumina'",
+        choices=["ont", "illumina"],
+        metavar="tech",
     )
     subparser_assemble.add_argument(
         "ref_fasta",
@@ -58,9 +81,8 @@ def main(args=None):
     )
     subparser_assemble.add_argument(
         "--minimap_opts",
-        help="Options string to pass to minimap2. Is used for initial mapping if reads provided with --reads_to_map, otherwise is ignored. Do not use -a or -o! This string is not sanity checked - it is up to you to provide valid options. Default is to use 1 thread and the Nanopore preset [%(default)s]",
+        help=f"Options string to pass to minimap2. Is used for initial mapping if reads provided with --reads_to_map, otherwise is ignored. Do not use -a or -o! This string is not sanity checked - it is up to you to provide valid options. Default is to use 1 thread and the Nanopore preset [{tech_dependent_usage_default_string('minimap_opts')}]",
         metavar="STRING",
-        default="-t 1 -x map-ont",
     )
     subparser_assemble.add_argument(
         "--min_mean_coverage",
@@ -71,8 +93,7 @@ def main(args=None):
     )
     subparser_assemble.add_argument(
         "--target_coverage",
-        help="Aim for this much coverage when extracting reads for polishing [%(default)s]",
-        default=500,
+        help=f"Aim for this much coverage when extracting reads for polishing [{tech_dependent_usage_default_string('target_coverage')}]",
         type=int,
         metavar="INT",
     )
@@ -92,8 +113,7 @@ def main(args=None):
     )
     subparser_assemble.add_argument(
         "--min_read_length",
-        help="Only use reads at least this long (after trimming with --read_end_trim). If the --wgs is used, then the reads must also overlap an 'amplicons' by at least this length to be used [%(default)s]",
-        default=200,
+        help=f"Only use reads at least this long (after trimming with --read_end_trim). If the --wgs is used, then the reads must also overlap an 'amplicons' by at least this length to be used [{tech_dependent_usage_default_string('min_read_length')}]",
         type=int,
         metavar="INT",
     )
@@ -178,6 +198,7 @@ def main(args=None):
     subparser_amp_over.set_defaults(func=viridian.tasks.amplicon_overlap.run)
 
     args = parser.parse_args()
+    set_tech_dependent_args(args)
 
     logging.basicConfig(
         format="[%(asctime)s viridian %(levelname)s] %(message)s",
